9291e9570471044dd5d4c268e9eb2e31f75a30cd
Sarthak <sarthak11yadav@gmail.com>
2021-07-29 23:22:30 +0530

    crmf

--- crm1/settings.py
+++ crm1/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticFfiles')
+
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
--- crm1/urls.py
+++ crm1/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.static import serve
+from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls'))
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 ]
